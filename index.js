fa49cb5ece7368390a9d073fa32e49eafedb12d7
dingzixuan <dingzixuan90@gmail.com>
2014-05-14 19:57:21 +0800

    building the middlewares stack

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
 var http = require("http");
 
 function express() {
+  
   function app(req, res) {
-    res.statusCode = 404;
-    res.end();
+    app.handle(req, res);
+  }
+  
+  app.queue = [];
+  app.handle = function(req, res, out) {
+    var index = 0;
+    var queue = this.queue;
+    function next(err) {
+      var nFn = queue[index++];
+      //结束
+      if (!nFn) {
+        if (out) {
+          return out(err);
+        }
+        //unhandled error should return 500
+        if (err) {
+          res.statusCode = 500;
+          res.end();
+        }
+        res.statusCode = 404;
+        res.end();
+        return;  //注意在这里return
+      }
+      try {
+        var arity = nFn.length;
+        if (err) {
+          //下个middleware有四个参数
+          if (arity == 4) {
+            nFn(err, req, res, next);
+          } else {
+            next(err);
+          }
+        } else if (arity < 4) {
+            nFn(req, res, next);
+        } else {
+            next();
+        }
+      } catch (e) {
+        //uncaught error
+        next(e);
+      }
+    }
+    next();
+  }
+  app.use = function(fn) {
+    //if ('function' == typeof fn) {
+    //  this.queue.push(fn);
+    //}
+    //sub app
+    if ('function' == typeof fn.handle) {
+      var server = fn;
+      fn = function(req, res, next) {
+        server.handle(req, res, next);
+      }
+    }
+    this.queue.push(fn);
+    return this;
   }
   app.listen = function() {
     var server = http.createServer(this);
