7bd38bb601d21ef830a0d1b5a6920dc568e2cb1e
dingzixuan <dingzixuan90@gmail.com>
2014-06-03 17:24:36 +0800

    Implement Prefix Path Trimming

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ var http = require("http");
 var Layer = require("./lib/layer");
 
 function express() {
-  
+
   //让app和app.handle参数一致，middleware就既可以是app也可以是function(req,res,next)
   function app(req, res, next) {
     app.handle(req, res, next);
   }
-  
+  app.isSubApp = false;
   app.queue = [];
   app.handle = function(req, res, out) {
     var index = 0;
     var queue = this.queue;
     function next(err) {
       var nLayer = queue[index++];
-      //结束
+      //从本app中结束
       if (!nLayer) {
-        if (out) {
-          return out(err);
+        //trim url 之前是在这里app调用结束后，调用out实现到另一个app的延续调用
+        // if (out) {
+        //   return out(err);
+        // }
+        //为了实现trim url，这里如果一个subapp调用完毕则直接return，在外层修改回url后再调用next实现延续调用
+        if (app.isSubApp) {
+          //err handle
         }
         //unhandled error should return 500
-        if (err) {
+        else if (err) {
           res.statusCode = 500;
+          res.write("500");
           res.end();
         } else {
           res.statusCode = 404;
+          res.write("404");
           res.end();
         }
         return;  //注意在这里return
@@ -34,11 +41,22 @@ function express() {
         //add req.params
         req.params = {};  //default value
         var reqInfo = nLayer.match(req.url);
-        if (!reqInfo) return next();  //如果url不匹配则跳过此middleware调用下一个
+        if (!reqInfo) { //如果url不匹配则跳过此middleware调用下一个
+          return next(err); 
+        }  
         req.params = reqInfo.params;
         var arity = nLayer.handle.length;
 
-        if (err) {  //pass err
+        //if middleware is a subapp, trim the req.url prefix and call handle
+        if ('function' == typeof nLayer.handle.handle) {
+          var oriUrl = req.url;
+          req.url = req.url.substring(reqInfo.path.length);
+          nLayer.handle(req, res, next);
+          req.url = oriUrl;
+          next(err);
+        }
+
+        else if (err) {  //pass err
           if (arity == 4) {
             nLayer.handle(err, req, res, next);
           } else {
@@ -56,23 +74,20 @@ function express() {
     }
     next();
   }
-  app.use = function(path, mw) {
+  app.use = function(path, mw, prefix) {
     var layer;
     //if has no path arg
     if ('string' != typeof path) {
       mw = path;
       path = '/';
     }
-    //不需要为subapp多加处理
-    //if fn is sub app
-    // if ('function' == typeof fn.handle) {
-    //   var server = fn;
-    //   fn = function(req, res, next) {
-    //     server.handle(req, res, next);
-    //   }
-    // }
+    prefix = prefix || false;
+    //if middleware is a subapp
+    if ('function' == typeof mw.handle) {
+      mw.isSubApp = true;
+    }
     //add layer to queue
-    layer = new Layer(path, mw);
+    layer = new Layer(path, mw, prefix);
     this.queue.push(layer);
     return this;
   }
